33ade639ec18caea44ab4e5d7e0384102480a7bf
chinthaka senarathna <chironprabath@gmail.com>
2025-04-20 21:15:13 +0530

    data types ???

---
+++ data_types/Hello.java
@@ -0,0 +1,18 @@
+public class Hello{
+    public static void main(String[] args) {
+        
+        byte num1 = 127;
+        short num2 = 223;
+        int num3 = 1124;
+        long num4 = 10123;
+
+        float num5 = 12.34f;
+        double num6 = 123.456;
+
+        char char1 = 'A';
+
+        boolean bool1 = true;
+        boolean bool2 = false;
+
+    }
+}
